7153f2d83f05850a6c47ec9477e8782df7fd7f3d
Admin <a04502633@gmail.com>
2026-02-25 17:15:46 +0530

    Environment Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/InsureAi': {
-        target: 'http://localhost:9090',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT),
+      proxy: {
+        '/InsureAi': {
+          target: 'http://localhost:9090',
+          changeOrigin: true
+        }
       }
     }
-  }
-})
-
-
-
-
-
+  };
+});
